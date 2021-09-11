3295bfc0f4072d91bc6e5bc5e88986ba1c066f5f
Jia-A <agrawaljiya2011@gmail.com>
2021-09-11 18:37:25 +0530

    file-beautified

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ var opFunc = document.querySelector("#text-op");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(text){
-    return serverURL+"?text="+text;
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
 }
 
-function errorHandler(error){
+function errorHandler(error) {
     console.log("Error detected : ", error);
     alert("Something went wrong, try again later.")
 }
-function clickHandler(){
+
+function clickHandler() {
     var inputText = ipFunc.value;
     fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translated;
-        opFunc.innerText = translatedText;
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            opFunc.innerText = translatedText;
         })
-    .catch(errorHandler)
+        .catch(errorHandler)
 }
 btnFunc.addEventListener("click", clickHandler);
